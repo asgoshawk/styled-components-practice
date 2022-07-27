b9e0b53455576e86e7ff8b0d3813ab37126d93a8
asgoshawk <x5553434@gmail.com>
2022-07-27 17:12:00 +0800

    First commit.

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import { StyledHeader, Nav, Logo, Image } from "./styles/Header.styled";
+import { Container } from "./styles/Container.styled";
+import { Flex } from "./styles/Flex.styled";
+import { Button } from "./styles/Button.styled";
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <Container>
+        <Nav>
+          <Logo src="./images/logo.svg" alt="" />
+          <Button>Try It Free</Button>
+        </Nav>
+        <Flex>
+          <div>
+            <h1>Title</h1>
+            <p>text here</p>
+            <Button bg="#ff0099" color="#fff">
+              Get Started For Free
+            </Button>
+          </div>
+
+          <Image src="./images/illustration-mockups.svg" alt="" />
+        </Flex>
+      </Container>
+    </StyledHeader>
+  );
+};
+
+export default Header;
